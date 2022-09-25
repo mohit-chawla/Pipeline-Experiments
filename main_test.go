716267206bd4fd1b821f766b086730e81f523d56
Mohit Chawla <mohitchawla.iitdhn@gmail.com>
2022-09-24 17:39:27 -0700

    env var test

--- main_test.go
+++ main_test.go
@@ -1,13 +1,20 @@
 package main
 
 import "testing"
+import "os"
 
 func TestAdd(t *testing.T){
 
     got := Add(4, 6)
     want := 11
 
+    for _, e := range os.Environ() {
+        pair := strings.SplitN(e, "=", 2)
+        fmt.Println(pair[0])
+    }
+    
     if got != want {
         t.Errorf("got %v, wanted %v", got, want)
     }
+
 }
